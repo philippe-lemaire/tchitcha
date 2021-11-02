3fd2b33266ff5b0e903184e6358a4b1997c71046
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-11-02 17:12:41 +0100

    basic views ok, index only showing future showings (including those of today)

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+import datetime
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Movie, Showing
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World, this is the index of the movies schedule.")
+    future_showing_list = Showing.objects.order_by("date").filter(
+        date__gte=datetime.date.today()
+    )
+    context = {"future_showing_list": future_showing_list}
+    return render(request, "movies/index.html", context)
+
+
+def detail(request, movie_id):
+    # detail view for a movie
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, "movies/details.html", {"movie": movie})
