8a9958ac4fdc5c9c5ccf7b22a085826aa49d4f3b
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-11-03 14:50:55 +0100

    better admin listings

--- movies/admin.py
+++ movies/admin.py
@@ -3,4 +3,16 @@ from .models import Movie, Room, Showing
 
 # Register your models here.
 
-admin.site.register([Movie, Room, Showing])
+
+class MovieAdmin(admin.ModelAdmin):
+    search_fields = ["title", "release_year"]
+    list_display = ["title", "rating", "genre", "release_year"]
+
+
+class ShowingAdmin(admin.ModelAdmin):
+    list_display = ["movie", "date", "time", "room", "tickets_sold"]
+
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Showing, ShowingAdmin)
+admin.site.register(Room)
