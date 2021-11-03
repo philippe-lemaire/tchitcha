f0aac03622e20ef70025afebeadbd6510177d8c4
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-11-03 10:52:45 +0100

    movie detail view with added context about showings

--- movies/views.py
+++ movies/views.py
@@ -10,3 +10,14 @@ from .models import Movie, Showing
 class IndexView(generic.ListView):
     def get_queryset(self):
         return Showing.objects.order_by("date").filter(date__gte=datetime.date.today())
+
+
+class DetailView(generic.DetailView):
+    model = Movie
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context["showing_list"] = Showing.objects.filter(movie=context.get("movie").pk)
+        return context
