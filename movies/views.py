891da4dab829ce3428c4479d19145029c82e2ecc
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-11-03 16:59:34 +0100

    showing detail view added, some urls rewired. templates used {% url %} tags so easy to switch urls

--- movies/views.py
+++ movies/views.py
@@ -12,7 +12,7 @@ class IndexView(generic.ListView):
         return Showing.objects.order_by("date").filter(date__gte=datetime.date.today())
 
 
-class DetailView(generic.DetailView):
+class MovieDetailView(generic.DetailView):
     model = Movie
 
     def get_context_data(self, **kwargs):
@@ -22,4 +22,16 @@ class DetailView(generic.DetailView):
         context["showing_list"] = Showing.objects.filter(
             movie=context.get("movie").pk
         ).filter(date__gte=datetime.date.today())
+        return context
+
+
+class ShowingDetailView(generic.DetailView):
+    model = Showing
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the future showings
+        showing = context.get("showing")
+        context["available_seats"] = showing.room.capacity - showing.tickets_sold
         return context
