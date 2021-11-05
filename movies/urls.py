1de3b0b650c93181c7b5b326d9915e866159dcdd
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-11-05 17:06:46 +0100

    removed showing detail view

--- movies/urls.py
+++ movies/urls.py
@@ -6,6 +6,5 @@ app_name = "movies"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("film/<int:pk>/", views.MovieDetailView.as_view(), name="detail"),
-    path("<int:pk>/", views.ShowingDetailView.as_view(), name="showing_detail"),
     path("billeterie/<int:showing_id>", views.buy_tickets, name="buy_tickets"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -29,18 +29,6 @@ class MovieDetailView(generic.DetailView):
         return context
 
 
-class ShowingDetailView(generic.DetailView):
-    model = Showing
-
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the future showings
-        showing = context.get("showing")
-        context["available_seats"] = showing.room.capacity - showing.tickets_sold
-        return context
-
-
 def buy_tickets(request, showing_id):
     showing = get_object_or_404(Showing, pk=showing_id)
     max_amount = showing.room.capacity - showing.tickets_sold
