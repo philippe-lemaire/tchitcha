8963bc43e8d4c886e2304a992a2bfad7c451405c
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-11-11 11:04:43 +0100

    new filter tag for duration

---
+++ movies/templatetags/movies_extras.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def duration(td):
+    total_seconds = int(td.total_seconds())
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+
+    return "{} h {}  min".format(hours, minutes)
